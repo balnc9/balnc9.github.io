340b37882ee7385aaf751c10ac85f0b42d196062
balnc9 <matthewlchin1@gmail.com>
2025-11-30 01:59:29 +0100

    template app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import "./index.css"
+
+import Dashboard from "./components/Dashboard";
+import IntroScreen from "./components/IntroScreen";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import PortfolioDisplay from "./components/PortfolioDisplay";
+import ContactSection from "./components/ContactSection";
+
+
 export default function App() {
         return (
-                <div>
-                        <h1>Hello world</h1>
-                </div>
+            <>
+                <Dashboard />
+
+                <section id ="intro" className = "section intro-section">
+                    <IntroScreen />
+                </section>
+
+                <section id ="about" className = "section about-section">
+                    <About />
+                </section>
+
+                <section id ="skills" className = "section skills-section">
+                    <Skills />
+                </section>
+
+                <section id="portfolio" className="portfolio-section">
+                    <PortfolioDisplay />
+                </section>
+
+                <section id="contact" className="section contact-section">
+                    <ContactSection />
+                </section>
+            </>
         );
 }
